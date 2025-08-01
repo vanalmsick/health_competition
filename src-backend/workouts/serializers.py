df58b647ca10eb08aae6bbb0d8f72042031d6d96
vanalmsick <44527602+vanalmsick@users.noreply.github.com>
2025-08-01 21:47:13 +0100

    Fixed: fix N+1 query warning and error from user__id #patch

--- src-backend/workouts/serializers.py
+++ src-backend/workouts/serializers.py
@@ -5,5 +5,5 @@ from .models import Workout
 class WorkoutSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workout
-        fields = ['id', 'user__id', 'sport_type', 'start_datetime', 'duration', 'duration_seconds', 'intensity_category', 'kcal', 'distance', 'strava_id']
-        read_only_fields = ['id', 'user__id', 'duration_seconds', 'strava_id'] #'duration_seconds',
+        fields = ['id', 'sport_type', 'start_datetime', 'duration', 'duration_seconds', 'intensity_category', 'kcal', 'distance', 'strava_id']
+        read_only_fields = ['id', 'duration_seconds', 'strava_id'] #'duration_seconds',
